39a670ed9d159be2d071fad19a29cea0671d4baf
yudhik11 <yudhik100@gmail.com>
2019-02-28 17:56:08 +0530

    Code updated

---
+++ src/demo.m
@@ -0,0 +1,53 @@
+clear all;
+
+param = Config(4,3);
+% 4 => Caltech
+% 3 => dslr
+[Data, Labels] = DataLoader(param.DATA_DIR);
+
+source_domain = param.source; 
+target_domain = param.target; 
+
+% Load data splits
+splits = load(param.result_filename);
+train_ids = splits.train;
+test_ids = splits.test;
+
+fprintf('Source Domain - %s, Target Domain - %s\n\n', ...
+    param.domain_names{source_domain}, param.domain_names{target_domain});
+
+% Store results:
+n = param.num_trials;
+telapsed = zeros(n,1);
+accuracy = zeros(n,1);
+pred_labels = cell(n,1);
+
+for i = 1:n
+    fprintf('       Iteration: %d / %d\n', i, n);
+    data.train.source = Data{source_domain}(train_ids.source{i}, :);
+    data.train.target = Data{target_domain}(train_ids.target{i}, :);
+    data.test.target = Data{target_domain}(test_ids.target{i}, :);
+    
+    labels.train.source = Labels{source_domain}(train_ids.source{i});
+    labels.train.target = Labels{target_domain}(train_ids.target{i});
+    labels.test.target = Labels{target_domain}(test_ids.target{i});
+    
+    if param.dim < size(data.train.source, 2)
+        P = pca([data.train.source; data.train.target; data.test.target], ...
+            'Economy', false);
+        data.train.source = data.train.source * P(:, 1:param.dim);
+        data.train.target = data.train.target * P(:, 1:param.dim);
+        data.test.target = data.test.target * P(:, 1:param.dim);
+    end
+    
+    tstart = tic;
+    [model_mmdt, W] = TrainMmdt(labels.train, data.train, param);
+    telapsed(i) = toc(tstart);
+       
+    [labels_output, score] = predict(model_mmdt, [data.test.target, ones(length(labels.test.target),1)]);
+    accuracy(i) = sum(labels_output == labels.test.target') / length(labels.test.target);    
+
+    fprintf('Accuracy = %.3f (Time = %6.2f)\n', accuracy(i), telapsed(i));
+end
+fprintf('\n\n Mean Accuracy = %.3f  (Mean time = %6.3f)\n', ...
+    mean(accuracy)*100.0, mean(telapsed));
